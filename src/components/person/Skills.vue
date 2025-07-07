89968f0c6545ce252fbeb0cbb4116a376782e290
PABLO VALENTIN <pablo.valentin@fatec.sp.gov.br>
2025-07-06 21:25:28 -0300

    Aprendendo a renderizar listas com o Vue.
    Utilizando a diretiva v-for para renderizar listas com o Vue.

--- src/components/person/Skills.vue
+++ src/components/person/Skills.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
     <h2>Habilidades:</h2>
+    <h3>Back-End:</h3>
     <ul>
       <!-- A diretiva "v-for" é utilizada para iterar os elementos de um array. -->
-      <li v-for="skill in skills">{{ skill }}</li>
+      <li v-for="(skill, index) in skills.backend" v-bind:key="index">{{ skill }}</li>
+    </ul>
+    <h3>Banco de Dados:</h3>
+    <ul>
+      <!-- A diretiva "v-for" é utilizada para iterar os elementos de um array. -->
+      <li v-for="skill in skills.database" :key="skill.id">{{ skill.description }}</li>
     </ul>
   </div>
 </template>
@@ -14,7 +20,13 @@
 
     data() {
       return {
-        skills: ["PHP", "C#", "JS", "SQL"]
+        skills: {
+          backend: ["PHP", "C#"],
+          database: [
+            {id: 1, description: "MySQL"},
+            {id: 2, description: "MongoDB"}
+          ]
+        }
       }
     }
   }
