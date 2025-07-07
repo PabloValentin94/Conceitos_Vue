de3df7a7fc63994d5a7ee63d45e40dd31b0c1046
PABLO VALENTIN <pablo.valentin@fatec.sp.gov.br>
2025-07-06 22:17:20 -0300

    Entendendo a diferença entre CSS Global e CSS Scoped.
    Utilizando o CSS que é aplicado a todos os elementos (Global) e o CSS específico de cada componente (Scoped).

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div>
     <Test/>
     <LifeCycle/>
+    <Header/>
     <Person/>
     <LifeCycle/>
     <Forms/>
@@ -11,6 +12,7 @@
 <script>
   import Test from "./components/Test.vue"
   import LifeCycle from "./components/LifeCycle.vue"
+  import Header from "./components/Header.vue"
   import Person from "./components/Person.vue"
   import Forms from "./components/Forms.vue"
 
@@ -20,8 +22,24 @@
     components: {
       Test,
       LifeCycle,
+      Header,
       Person,
       Forms
     }
   }
 </script>
+
+<!-- CSS Global -->
+<style>
+  h1, h2, h3, p, ul li {
+    color: #FFFFFF;
+  }
+
+  body {
+    background-color: #202020;
+  }
+
+  a {
+    color: #FFFF00;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul>
+    <li><a href="#">Portfólio</a></li>
+    <li><a href="#">Habilidades</a></li>
+    <li><a href="#">Contato</a></li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: "Header"
+  }
+</script>
+
+<!-- CSS Scoped (Específico) -->
+<style scoped>
+  ul {
+    list-style: none;
+    display: flex;
+  }
+
+  ul li {
+    margin-right: 20px;
+  }
+
+  ul li a {
+    color: #FFFFFF;
+    text-decoration: none;
+    transition: 0.5s;
+  }
+
+  ul li a:hover {
+    color: #FF0000;
+  }
+</style>
